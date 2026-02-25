a47497c6a3b3b2f9af337dc751e952b834e68a83
Billy Lee <billylee@billys-air.mynetworksettings.com>
2026-02-25 16:30:19 -0600

    completley lost

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css'
-import Footer from './components/Footer';
+import "./App.css";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Books from './pages/Books';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+// import BooksInfo from "./pages/BookInfo.jsx";
+import { books } from "./data.js"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact render={<Books books={books} />} />
+          {/* <Route path="/books/.id" exact element={<BookInfo books={books} />} /> */}
+        </Routes>
         <Footer />
       </div>
     </Router>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,28 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+// import { books } from '../data'
+import Book from "../components/ul/Book";
 
-const Books = () => {
+const Books = ({ books: initialBooks }) => {
+  const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks() {
+    console.log('filter')
+  }
   return (
     <div id="books__body">
-        <main id="books__main">
-            <section>
-                <div className="books__container">
-                    <div className="row">
-                        <div className="books__header">
-                            <h2 className='section__title books__header--title'>All Books</h2>
-                            <select id="filter">
-                                <option value="" selected disabled>Sort</option>
-                                <option value="Low_to_High">Price, Low to High</option>
-                                <option value="High_to_Low">Price, High to Low</option>
-                                <option value="Rating">Rating</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-      
+      <main id="books__main">
+        <section>
+          <div className="books__container">
+            <div className="row">
+              <div className="books__header">
+                <h2 className="section__title books__header--title">
+                  All Books
+                </h2>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={filterBooks}
+                >
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="Low_to_High">Price, Low to High</option>
+                  <option value="High_to_Low">Price, High to Low</option>
+                  <option value="Rating">Rating</option>
+                </select>
+              </div>
+              <div className="books">
+                {books.map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
