e82b0e05383bcf6e766a0b8933a0425b2ed5d8e9
Billy Lee <billylee@billys-air.mynetworksettings.com>
2026-03-13 19:10:17 -0500

    no book img

--- src/components/ul/Book.jsx
+++ src/components/ul/Book.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
@@ -7,25 +7,30 @@ import Price from "./Price";
 const Book = ({ book }) => {
   const [img, setImg] = useState();
 
+  const mountedRef = useRef(true);
+
   useEffect(() => {
     const image = new Image();
     image.src = book.url;
     image.onload = () => {
-      setImg(image);
-    }
-  })
-  
+      setTimeout(() => {
+        if (mountedRef.current) {
+          setImg(image);
+        }
+      }, 300);
+    };
+    return () => {
+      mountedRef.current = false;
+    };
+  });
+
   return (
     <div className="book">
       {img ? (
         <>
           <Link to={`/books/${book.id}`}>
             <figure className="book__img--wrapper">
-              <img
-                src={img.src}
-                alt=""
-                className="book__img"
-              />
+              <img src={img.src} alt="" className="book__img" />
             </figure>
           </Link>
           <div className="book__title">
@@ -40,12 +45,13 @@ const Book = ({ book }) => {
           />
         </>
       ) : (
-        <> </>
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>{" "}
+        </>
       )}
-      <div className="book__img--skeleton"></div>
-      <div className="skeleton book__title--skeleton"></div>
-      <div className="skeleton book__rating--skeleton"></div>
-      <div className="skeleton book__price--skeleton"></div>
     </div>
   );
 };
